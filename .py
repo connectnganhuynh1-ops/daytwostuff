4d64d2ec25411b37a04a345d601d80b501a06945
connectnganhuynh1-ops <connect.nganhuynh1@gmail.com>
2026-01-15 20:48:16 +0000

    Add dataset loading and requirements file

---
+++ .py
@@ -0,0 +1,17 @@
+# %%
+# 1) What is your terminal display "path"? (type/paste as text into the .py file)
+# /workspaces/daytwostuff
+# 2) Should you include the environment in your repo or not?
+# No,you should not include your environment in your repo, it's better to use a requirements.txt file to specifcy dependencies. 
+# 3) Now what is your terminal display "path"? Is it different?
+# (venv)/workspaces/daytwostuff, it is similar but slightly different with the venv
+# 4) What do you notice about the extension menu?
+# You can search for extensions and see ratings and reviews for each extension. It also shows if extensions are installed or enabled. 
+# 5) Note the version
+# 6.5.2
+# 6) Review the capabilities, what are three useful elements of Data Wrangler
+# 1. Data Cleaning
+# 2. Data Transformation
+# 3. Data Visualization
+# 7) Why do we use a requirements.txt file?
+# It allows us to file to record and share the Python dependencies a project needs. 
